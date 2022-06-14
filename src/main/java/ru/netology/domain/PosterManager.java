9af5d258c0671b931d5e2316abdcc3aef7ef1d4a
Tatiana Lyapina <antipts@gmail.com>
2022-06-14 21:04:08 +0300

    Add Mockito

--- src/main/java/ru/netology/domain/PosterManager.java
+++ src/main/java/ru/netology/domain/PosterManager.java
@@ -5,40 +5,42 @@ import lombok.NoArgsConstructor;
 import ru.netology.domain.WatchMovie;
 
 
-@NoArgsConstructor
 public class PosterManager {
 
     private WatchMovie[] movies = new WatchMovie[0];
-    private int resultLength = 10;
 
-    public PosterManager (int maxAdded) {
-        this.resultLength = maxAdded;
-    }
+    private PosterRepository repository;
 
-    public void add (WatchMovie movie) {
-        int length = movies.length + 1;
-        WatchMovie[] tmp = new WatchMovie[length];
-        System.arraycopy (movies, 0, tmp, 0, movies.length);
-        int lastIndex = tmp.length - 1;
-        tmp [lastIndex] = movie;
-        movies = tmp;
+    private int maxAdded = 10;
+    public PosterManager( PosterRepository repository) {this.repository = repository; }
+    public PosterManager(int resultLength, PosterRepository repository) {this.repository = repository;
+    this.maxAdded = resultLength;}
+
+    public void add (WatchMovie movie) {repository.save(movie); }
+    public WatchMovie[] findAll() {
+        return repository.findAll();
     }
-    public WatchMovie[] findAll() { return movies; }
 
     public WatchMovie[] findLast() {
         WatchMovie[] movies = findAll();
-        if (movies.length < resultLength) {
-            resultLength = movies.length;}
+        if (movies.length < maxAdded) {
+            maxAdded = movies.length;}
         else {
-            resultLength = this.resultLength;
+            maxAdded = this.maxAdded;
         }
-        WatchMovie[] result = new WatchMovie[resultLength];
+        WatchMovie[] result = new WatchMovie[maxAdded];
         int i;
-        for ( i=0; i < resultLength; i++){
+        for ( i=0; i < maxAdded; i++){
             int index  = movies.length - i - 1;
             result[i] = movies[index];
         }
         return  result;
 
     }
+    public void findById(int id) {repository.findById(id);
+    }
+    public void removeById(int id) {repository.removeById(id);
+    }
+
+    public void removeAll() {repository.removeAll(); }
 }
---
+++ src/test/java/ru/netology/domain/PosterManagerMockitoTest.java
@@ -0,0 +1,33 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.netology.domain.PosterRepository;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+public class PosterManagerMockitoTest {
+
+    private PosterRepository repository = Mockito.mock(PosterRepository.class);
+    private PosterManager manager = new PosterManager (4, repository);
+    private WatchMovie first = new WatchMovie(145, "http://1", "Аллея кошмаров", "20 января 2022", "Драма");
+    private WatchMovie second = new WatchMovie(15, "http://2", "Анчартед: На картах не значится", "8 февраля 2022", "Приключения");
+    private WatchMovie third = new WatchMovie(367, "http://3", "Смерть на Ниле", "10 февраля 2022", "Детектив");
+    private WatchMovie fourth = new WatchMovie(862, "http://4", "Я краснею", "01 марта 2022", "Мультфильм");
+    private WatchMovie fifth = new WatchMovie(834, "http://5", "Фантастические твари: Тайны Дамблдора", "6 апреля 2022", "Фентези");
+
+    @Test
+    public void findLastTest() {
+        WatchMovie[] returned = new WatchMovie[] {first, second, third, fourth, fifth};
+        doReturn(returned).when(repository).findAll();
+
+
+        WatchMovie[] actual = manager.findLast();
+        WatchMovie[] expected = { fifth, fourth, third, second };
+        Assertions.assertArrayEquals(actual, expected);
+        verify(repository).findAll();
+    }
+
+}
